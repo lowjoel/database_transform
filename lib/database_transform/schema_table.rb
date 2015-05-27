d883f55eed29f9ffdc4d42e8cd0b9dbbccfa068e
Joel Low <joel@joelsplace.sg>
2015-05-27 15:53:31 +0800

    Define column mappings and primary key declarations.

--- lib/database_transform/schema_table.rb
+++ lib/database_transform/schema_table.rb
@@ -9,6 +9,52 @@ class DatabaseTransform::SchemaTable
     @source = source
     @destination = destination
     @default_scope = default_scope
-    @columns = {}
+    @columns = []
+  end
+
+  # Declare the primary key of the source table.
+  #
+  # @param [Symbol] id The name of the column in the source table which is the primary key.
+  def primary_key(id)
+    raise DatabaseTransform::DuplicateError.new(id) if @primary_key
+    @primary_key = id
+    @source.primary_key = id
+  end
+
+  # Declare the mapping for the source table to the new table.
+  #
+  # This function takes source columns to provide to the mapping proc as arguments.
+  #
+  # The destination column is specified as the to hash parameter.
+  # If no mapping block is specified, the source column is copied to the destination column without modification. This
+  # is not possible if more than one source column is specified.
+  # If the mapping block is specified, if the destination column is specified, the result of the block is used as the
+  # destination column's value.
+  # Otherwise, the result is unused. This can be used to execute the block purely for side-effects
+  #
+  # @option options [Symbol] to The column to map to.
+  # @option options [Boolean] null If to is specified, this can be false which enforces that nil is never added to the
+  #   database.
+  def column(*args, &block)
+    raise ArgumentError if args.length < 1
+
+    # Get the source columns
+    if args[args.length - 1].is_a?(Hash)
+      source_columns = args.take(args.length - 1)
+      args = args.extract_options!
+    else
+      source_columns = args
+      args = {}
+    end
+
+    raise ArgumentError.new unless args[:to].nil? || args[:to].is_a?(Symbol)
+
+    # Store the mapping
+    @columns << {
+        from: source_columns,
+        to: args[:to],
+        null: args[:null].nil? ? true : args[:null],
+        block: block
+    }
   end
 end
--- spec/database_transform/schema_table_spec.rb
+++ spec/database_transform/schema_table_spec.rb
@@ -2,7 +2,22 @@ require 'spec_helper'
 
 RSpec.describe DatabaseTransform::SchemaTable do
   class DummyTableSchema < DatabaseTransform::Schema; end
-  subject { DatabaseTransform::SchemaTable.new(DummyTableSchema, source_table, default_scope) }
-  let(:source_table) { nil }
+  subject { DatabaseTransform::SchemaTable.new(source_model, destination_model, default_scope) }
+  let(:source_model) { Source }
+  let(:destination_model) { Destination }
   let(:default_scope) { nil }
+
+  describe '#primary_key' do
+    it 'sets the primary key for the source table' do
+      subject.primary_key :id
+      expect(subject.instance_variable_get(:@primary_key)).to eq(:id)
+    end
+  end
+
+  describe '#column' do
+    it 'declares the mapping for the given column' do
+      subject.column :name
+      expect(subject.instance_variable_get(:@columns).find { |c| c[:from] == [:name] }).to be_truthy
+    end
+  end
 end
